ed34aa5a7d0e0216c6c943d1af949d5641ee0e10
laurent <lfran@broadinstitute.org>
2017-07-17 12:11:03 -0400

    Changed coverage normalization to min-max

--- encode_coverage.py
+++ encode_coverage.py
@@ -10,7 +10,7 @@ import sys
 def main(args):
 
     contigs = {}
-
+    max_coverage = 0
     for input in args.input:
         for file in glob.glob(input):
             print "Loading coverage from file: {}".format(file)
@@ -20,29 +20,34 @@ def main(args):
             coverage = np.zeros(df['pos'].iloc[-1], dtype=np.float32)
             coverage[df['pos'].values - 1] = df['mean'].values
             contigs.update({df['#chrom'].iloc[0] : coverage})
+            max_coverage = max(max_coverage, np.max(df['mean'].values))
 
-    print "Normalizing coverage using {} points to compute the norm".format(args.n_points_for_norm)
+    # print "Normalizing coverage using {} points to compute the norm".format(args.n_points_for_norm)
+    # for contig, coverage in contigs.iteritems():
+    #     print "Max {} coverage: {}".format(contig, np.max(coverage))
+    #
+    # total_length = float(sum([len(x) for x in contigs.values()]))
+    # print total_length
+    # p_per_base = args.n_points_for_norm / total_length
+    # coverage_sample = np.asarray([np.random.choice(x, int(p_per_base * len(x))) for x in contigs.values()])
+    #max = np.max(coverage_sample)
+    #norm = np.linalg.norm(np.asarray([ np.random.choice(x, int(p_per_base * len(x))) for x in contigs.values() ]).flatten())
 
-    total_length = float(sum([len(x) for x in contigs.values()]))
-    print total_length
-    p_per_base = args.n_points_for_norm / total_length
-    norm = np.linalg.norm(np.asarray([ np.random.choice(x, int(p_per_base * len(x))) for x in contigs.values() ]).flatten())
-
-    print "Norm: {}".format(norm)
+    print "Max coverage value found: {}".format(max_coverage)
 
     print "Writing coverage"
     if not path.exists(args.output):
         makedirs(args.output)
 
     for contig, coverage in contigs.iteritems():
-        np.save(args.output + "/{}.npy".format(contig), (coverage / norm).astype(np.float16))
+        np.save(args.output + "/{}.npy".format(contig), (coverage / max_coverage).astype(np.float16))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", help="Coverage file(s).", nargs='+')
     parser.add_argument("--output", help="Coverage output path.")
-    parser.add_argument("--n_points_for_norm", help="Number of coverage points to use for normalization.", type=int, default=50000000)
+    parser.add_argument("--n_points_for_norm", help="Number of coverage points to use for normalization. (default 50M)", type=int, default=50000000)
 
     main(parser.parse_args())
 
