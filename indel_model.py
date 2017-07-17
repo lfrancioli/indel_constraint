745d9ed8b9de5e754a49ed255ee174f043436f63
laurent <lfran@broadinstitute.org>
2017-07-17 13:19:13 -0400

    Added coverage to model

--- indel_model.py
+++ indel_model.py
@@ -25,8 +25,10 @@ from Bio import SeqIO
 from keras.optimizers import SGD, Adam
 from keras.models import Model
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
-from keras.layers import Input, Dense, Dropout, Flatten, Reshape, merge, SpatialDropout1D
+from keras.layers import Input, Dense, Dropout, Flatten, Reshape, merge, SpatialDropout1D, concatenate
+from keras import backend as K
 from keras.layers.convolutional import Conv1D, MaxPooling1D
+import tensorflow as tf
 
 logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
 logger = logging.getLogger("indel")
@@ -123,6 +125,18 @@ def load_bitpacked_reference(in_path):
     logger.info("Reference loaded in {}.".format(timer() - start))
     return reference, ambiguous_bases
 
+def load_coverage(in_path):
+    start = timer()
+    logger.info("Loading coverage files from {}".format(in_path))
+    coverage = {}
+    for f in listdir(in_path):
+        if f.endswith(".npy"):
+            coverage_values = np.load(in_path + "/" + f)
+            coverage[f[:-4]] = coverage_values.reshape(len(coverage_values),1)
+
+    logger.info("Coverage files loaded in {}".format(timer() - start))
+    return coverage
+
 
 def main(args):
     if args.ref_fasta:
@@ -131,45 +145,58 @@ def main(args):
     else:
         reference, ambiguous_bases = load_bitpacked_reference(args.ref)
 
-    if args.training_vcf is not None:
-        training_indels, training_indel_contigs = read_vcfs(args.training_vcf, args.max_training, args.pass_only)
+    if args.train_vcf is not None:
+        training_indels, training_indel_contigs = read_vcfs(args.train_vcf, args.max_training, args.pass_only)
 
-    if args.eval_indels is not None:
+    if args.eval_vcf is not None:
         eval_indels, eval_indel_contigs = read_vcfs(args.eval_indels, args.max_training, args.pass_only)
-    elif args.training_vcf is not None:
+    elif args.train_vcf is not None:
         eval_indels, eval_indel_contigs = training_indels, training_indel_contigs
 
+    if args.train_model or args.save_best_examples or args.compute_predictions:
+        coverage = load_coverage(args.coverage)
+
     if args.train_model or args.save_best_examples:
-        logger.info("Loading training data")
-        training, labels, indel_indices = vcf_to_indel_tensors(training_indels, reference, ambiguous_bases, args.window_size, args.neg_train_window, args.neg_to_pos_training)
-        logger.info("Loaded training examples with dimensions {}, labels with dimensions: {} and indel indices with dimesions: {}.".format(str(training.shape), str(labels.shape), str(indel_indices.shape)))
+        logger.info("Creating training tensors")
+        training_bases, training_coverage, labels, indel_indices = vcf_to_indel_tensors(training_indels,
+                                                               reference,
+                                                               ambiguous_bases,
+                                                               coverage,
+                                                               args.window_size,
+                                                               args.neg_train_window,
+                                                               args.neg_to_pos_training)
+        logger.info("Training tensors created with dimensions:\ntraining_bases: {}\ntraining coverage:{}\nlabels: {}\nindel indices: {}.".format(training_bases.shape, training_coverage.shape, labels.shape, indel_indices.shape))
 
         logger.info("Splitting training data into training and testing sets.")
-        train, test = split_data([training, labels, indel_indices], [0.8,0.2])
+        train, test = split_data([training_bases, training_coverage, labels, indel_indices], [0.8,0.2])
 
-        logger.info("Training shape: {}, labels shape: {}".format(train[0].shape, train[1].shape))
+        logger.info("Training bases shape: {}, training coverage shape: {}, labels shape: {}".format(train[0].shape, train[1].shape, train[2].shape))
 
-
+    K.set_learning_phase(1)
     model = make_indel_model(args.window_size)
 
     if args.train_model:
-        logger.info("Training model.")
+        K.set_learning_phase(1)
+        logger.info("Training model. Learning phase =  {}".format(K.learning_phase()))
         if args.callback:
             model.load_weights(args.callback, by_name=True)
-        history = model.fit(train[0], train[1], batch_size=32, validation_split=0.2, shuffle=True, epochs=10, callbacks=get_callbacks(args.output+".callbacks"))
+        history = model.fit([train[0], train[1]], train[2], batch_size=32, validation_split=0.2, shuffle=True, epochs=10, callbacks=get_callbacks(args.output+".callbacks"))
 
         logger.info("Plotting metrics.")
         plot_metric_history(history, args.output + ".metrics_history.pdf", "Indel model")
 
-        logger.info("Evaluating model.")
-        eval_values = model.evaluate(test[0], test[1])
+        K.set_learning_phase(0)
+        logger.info("Evaluating model. Learning phase =  {}".format(K.learning_phase()))
+        eval_values = model.evaluate([test[0], test[1]], test[2])
         pprint(", ".join(["{}: {}".format(metric, value) for metric, value in zip(model.metrics_names, eval_values)]))
 
     else:
         model.load_weights(args.callback, by_name=True)
 
+        K.set_learning_phase(0)
+
     if args.save_best_examples:
-        logger.info("Saving best scoring training indel examples")
+        logger.info("Saving best scoring training indel examples. Learning phase =  {}".format(K.learning_phase()))
         positive_examples, negative_examples = get_best_scoring_training_examples(model, train)
         with open(args.output + ".best_training_examples.tsv",'w') as file:
             for seq, score, label, indel_index in positive_examples + negative_examples:
@@ -193,7 +220,7 @@ def main(args):
         for contig in reference.keys():
             if pred_contig is None or pred_contig == contig:
                 reference_contig = reference[contig] if pred_end is None else reference[contig][0:int(pred_end)]
-                results = apply_model(model, reference_contig, ambiguous_bases[contig], args.window_size)
+                results = apply_model(model, reference_contig, ambiguous_bases[contig], coverage[contig], args.window_size)
                 predictions[contig] = results
                 np.save(predictions_path + "{}.npy".format(contig), results)
 
@@ -261,9 +288,11 @@ def read_vcf(vcf_path, max_training, pass_only):
 
     return indels, contigs
 
-def vcf_to_indel_tensors(indels, reference, ambiguous_bases, window_size, neg_train_window, neg_to_pos_training, squash_multi_allelic = False):
-    positive_training = []
-    negative_training = []
+def vcf_to_indel_tensors(indels, reference, ambiguous_bases, coverage, window_size, neg_train_window, neg_to_pos_training, squash_multi_allelic = False):
+    positive_training_bases = []
+    negative_training_bases = []
+    positive_training_coverage = []
+    negative_training_coverage = []
     labels = np.empty((0,2),dtype=np.int8)
     indel_indices = []
 
@@ -275,7 +304,8 @@ def vcf_to_indel_tensors(indels, reference, ambiguous_bases, window_size, neg_tr
         #Get positive training examples
         if not variant.is_snp and not (squash_multi_allelic and variant.POS in positive_training_pos):
             pos = variant.POS
-            positive_training.append(reference[contig][pos - window_size -1: pos + window_size])
+            positive_training_bases.append(reference[contig][pos - window_size -1: pos + window_size])
+            positive_training_coverage.append(coverage[contig][pos - window_size -1: pos + window_size])
             indel_indices.append(i)
             while pos in positive_training_pos: # A bit of a trick for multi-allelic indels, but should be fine
                 pos+=1
@@ -284,13 +314,16 @@ def vcf_to_indel_tensors(indels, reference, ambiguous_bases, window_size, neg_tr
         if i+1 == len(indels) or indels[i+1].CHROM != variant.CHROM:
             print i
             # Get negative training examples
-            negative_training.extend(get_negative_training_tensors(positive_training_pos,
-                                                     ambiguous_bases[contig],
-                                                     neg_train_window,
-                                                     reference[contig],
-                                                     window_size,
-                                                     neg_to_pos_training
-                                                     ))
+            neg_positions = get_negative_training_positions(positive_training_pos,
+                                                                           ambiguous_bases[contig],
+                                                                           len(reference[contig]) - window_size,
+                                                                           neg_train_window,
+                                                                           window_size,
+                                                                           neg_to_pos_training
+                                                                           )
+            negative_training_bases.extend([reference[contig][neg_pos - window_size -1: neg_pos + window_size] for neg_pos in neg_positions])
+            negative_training_coverage.extend(
+                [coverage[contig][neg_pos - window_size - 1: neg_pos + window_size] for neg_pos in neg_positions])
 
             indel_indices.extend([-1]*len(positive_training_pos) * neg_to_pos_training)
             labels = np.append(
@@ -308,7 +341,7 @@ def vcf_to_indel_tensors(indels, reference, ambiguous_bases, window_size, neg_tr
 
     logger.info("Training examples loaded in {}.".format(timer() - start))
 
-    return np.asarray(positive_training + negative_training), labels, np.asarray(indel_indices)
+    return np.asarray(positive_training_bases + negative_training_bases), np.asarray(positive_training_coverage + negative_training_coverage), labels, np.asarray(indel_indices)
 
 def one_hot_encode(data, encoder):
     all_data = "".join(data)
@@ -322,8 +355,7 @@ def get_training_tensor(chrom, pos, reference, window_size, encoder):
     return encoder.transform(list(record))
 
 
-def get_negative_training_tensors(positions, ambiguous_bases, neg_train_window, reference, window_size, neg_to_pos_training, max_retry = 100):
-    max_pos = len(reference) - window_size
+def get_negative_training_positions(positions, ambiguous_bases, max_pos, neg_train_window, window_size, neg_to_pos_training, max_retry = 100):
     neg_positions = []
     if neg_train_window > 0:
         for pos in positions.keys():
@@ -347,13 +379,17 @@ def get_negative_training_tensors(positions, ambiguous_bases, neg_train_window, 
                                  if x not in positions and not in_intervals(ambiguous_bases, x)]
             neg_positions.extend(new_neg_positions)
 
-    return [reference[neg_pos - window_size -1: neg_pos + window_size] for neg_pos in neg_positions]
+    return neg_positions
 
 
 def make_indel_model(window_size):
     indel = Input(shape=(2 * window_size + 1, len(BASES)), name="indel")
+    coverage = Input(shape=(2 * window_size + 1, 1), name="coverage")
     x = Conv1D(filters=200, kernel_size=12, activation="relu", kernel_initializer='glorot_normal')(indel)
     x = SpatialDropout1D(0.2)(x)
+    y = Conv1D(filters=32, kernel_size=12, activation="relu", kernel_initializer='glorot_normal')(coverage)
+    y = SpatialDropout1D(0.2)(y)
+    x = concatenate([x,y])
     x = Conv1D(filters=200, kernel_size=12, activation="relu", kernel_initializer='glorot_normal')(x)
     x = Dropout(0.2)(x)
     x = Conv1D(filters=100, kernel_size=12, activation="relu", kernel_initializer='glorot_normal')(x)
@@ -362,7 +398,7 @@ def make_indel_model(window_size):
     x = Dense(units=64, kernel_initializer='normal', activation='relu')(x)
     prob_output = Dense(units=2, kernel_initializer='normal', activation='softmax')(x)
 
-    model = Model(inputs=[indel], outputs=[prob_output])
+    model = Model(inputs=[indel, coverage], outputs=[prob_output])
 
     sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=0.5)
     adamo = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, clipnorm=1.)
@@ -529,7 +565,7 @@ def get_best_scoring_training_examples(model, training_data, n_best = 50):
     return positive_examples, negative_examples
 
 
-def apply_model(model, reference_contig, ambiguous_bases_contig, window_size):
+def apply_model(model, reference_contig, ambiguous_bases_contig, coverage_contig, window_size):
     logger.info("Computing predictions for {} bases.".format(len(reference_contig)))
     start = timer()
     intervals = get_interval_overlaps(ambiguous_bases_contig, window_size, len(reference_contig) - window_size)
@@ -538,9 +574,12 @@ def apply_model(model, reference_contig, ambiguous_bases_contig, window_size):
         if overlaps:
             predictions = np.append(predictions, np.zeros(interval_stop - interval_start, dtype=np.float16))
         else:
-            tensors = np.asarray([ reference_contig[i - window_size -1: i + window_size] for
+            tensors_bases = np.asarray([ reference_contig[i - window_size -1: i + window_size] for
                         i in range(interval_start, interval_stop)]) #This will have N context around start/end of non-amibugous sequences -- OK?
-            predictions = np.append(predictions, model.predict(tensors, verbose=1)[:,0].astype(np.float16))
+            tensors_coverage = np.asarray([coverage_contig[i - window_size - 1: i + window_size] for
+                                        i in range(interval_start,
+                                                   interval_stop)])  # This will have N context around start/end of non-amibugous sequences -- OK?
+            predictions = np.append(predictions, model.predict([tensors_bases, tensors_coverage], verbose=1)[:,0].astype(np.float16))
 
     predictions = np.append(predictions, np.zeros(window_size, dtype=np.float16))
     logger.info("Computed {} predictions in {}.".format(len(predictions), timer() - start))
@@ -631,8 +670,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--ref', help='One-hot-encoded reference (required if not using --ref_fasta)', required=False)
     parser.add_argument('--ref_fasta', help='Reference fasta. If used, the reference will be one-hot-encoded and saved as a npy files. (required if not using --ref)', required=False)
-    parser.add_argument('--training_vcf', help='VCF(s) containing indel training examples. Required to train a model.', required=False, nargs="+")
-    parser.add_argument('--eval_vcf', help='VCF(s) containing indel evaluation examples. If not specified, VCF used for training is used. Either --training_vcf or --eval_vcf is required for evaluation.', required=False, nargs="+")
+    parser.add_argument('--coverage', help='Normalized coverage npy files location', required=False)
+    parser.add_argument('--train_vcf', help='VCF(s) containing indel training examples. Required to train a model.', required=False, nargs="+")
+    parser.add_argument('--eval_vcf', help='VCF(s) containing indel evaluation examples. If not specified, VCF used for training is used. Either --train_vcf or --eval_vcf is required for evaluation.', required=False, nargs="+")
     parser.add_argument('--output', help='Output prefix', required=True)
     parser.add_argument('--window_size', help='Size of the window to consider on each side of the indel (default 30).',
                         required=False, default=30, type=int)
@@ -676,11 +716,11 @@ if __name__ == '__main__':
     if args.compute_predictions and args.load_predictions:
         sys.exit("Only one of --compute_predictions and --load_predictions can be specified.")
 
-    if (args.train_model or args.save_best_examples) and (args.training_vcf is None):
-        sys.exit("--training_vcf needs to be specified to train a model (--train_model) or save best training examples (--save_best_examples).")
+    if (args.train_model or args.save_best_examples) and (args.train_vcf is None):
+        sys.exit("--train_vcf needs to be specified to train a model (--train_model) or save best training examples (--save_best_examples).")
 
-    if args.eval_predictions and args.training_vcf is None and args.eval_vcf is None:
-        sys.exit("At least one of --training_vcf or --eval_vcf needed for evaluation of predictions (--eval_predictions).")
+    if args.eval_predictions and args.train_vcf is None and args.eval_vcf is None:
+        sys.exit("At least one of --train_vcf or --eval_vcf needed for evaluation of predictions (--eval_predictions).")
 
     if args.eval_bin_size < 1:
         sys.exit("--eval_bin_size value must be > 0.")
